e7732b658951f26ca6b2904d06f4e147b846b118
Lufrut <superkolasar5@gmail.com>
2023-08-17 13:22:38 +0200

    finish task

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -4,7 +4,43 @@ import java.util.ArrayDeque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        firstQueue = new ArrayDeque<>();
+        firstQueue.add(1);
+        firstQueue.add(5);
+        firstQueue.add(7);
+        firstQueue.add(6);
+        secondQueue = new ArrayDeque<>();
+        secondQueue.add(2);
+        secondQueue.add(5);
+        secondQueue.add(8);
+        secondQueue.add(3);
+
+        ArrayDeque<Integer> out = new ArrayDeque<>();
+        out.add(firstQueue.remove());
+        out.add(firstQueue.remove());
+
+        out.add(secondQueue.remove());
+        out.add(secondQueue.remove());
+
+        while(!firstQueue.isEmpty() && !secondQueue.isEmpty()){
+
+
+            firstQueue.add(out.remove());
+
+            out.add(firstQueue.remove());
+            out.add(firstQueue.remove());
+
+            secondQueue.add(out.remove());
+
+            out.add(secondQueue.remove());
+            out.add(secondQueue.remove());
+
+        }
+        System.out.println(out);
+        return out;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,21 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        List<Integer> out = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) queue.add(i);
+        while(!queue.isEmpty()){
+            for(int i = 1; i < everyDishNumberToEat; i++){
+                queue.add(queue.remove());
+            }
+            out.add(queue.remove());
+        }
+        return out;
     }
 }
